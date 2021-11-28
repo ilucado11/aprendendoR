cfc519bfe596a3cdfe109678eeaccff3c96b63a9
ilucado11 <claudio_farias1901@hotmail.com>
2021-11-27 21:27:52 -0300

    First Commit

---
+++ R/somar.R
@@ -0,0 +1,15 @@
+#'Funcao para somar
+#'
+#'Esta funcao serve para somar dois numeros
+#'
+#' @param a um numero
+#' @param b outro numero
+#'
+#' @examples
+#' somar(3,4)
+#'
+#' @export
+
+somar <- function(a,b){
+  a + b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,16 @@
+#'Funcao para subtrair
+#'
+#'Esta funcao serve para subtrair dois numeros
+#'
+#' @param a um numero
+#' @param b outro numero
+#'
+#' @examples
+#' subtrair(3,4)
+#'
+#' @export
+
+subtrair <- function(a, b){
+  a - b
+}
+
